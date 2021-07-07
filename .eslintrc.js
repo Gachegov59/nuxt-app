794466f93297258e4881cea702602e913245001e
alex.g <gachegov59@gmail.com>
2021-07-07 09:49:34 +0500

    practice

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,28 @@
 module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    node: true,
-  },
-  parserOptions: {
-    parser: 'babel-eslint',
-  },
-  extends: [
-    '@nuxtjs',
-    'plugin:prettier/recommended',
-    'plugin:nuxt/recommended',
-  ],
-  plugins: [],
-  // add your custom rules here
-  rules: {},
-}
+	root: true,
+	env: {
+		node: true,
+		browser: true
+	},
+	extends: ['plugin:vue/recommended', 'eslint:recommended', 'plugin:prettier/recommended'],
+	rules: {
+		'vue/component-name-in-template-casing': ['error', 'PascalCase'],
+		'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+		'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+		'vue/max-attributes-per-line': 'off',
+		'no-tabs': 'off',
+		'no-useless-constructor': 'off',
+		'require-await': 'off',
+		'prefer-const': 'off',
+		'import/no-mutable-exports': 'off',
+		'no-use-before-define': 'off',
+		'nuxt/no-cjs-in-config': 'off',
+		camelcase: 'off'
+	},
+	globals: {
+		$nuxt: true
+	},
+	parserOptions: {
+		parser: 'babel-eslint'
+	}
+};
---
+++ components/AppInput.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+	div
+		input(preloader="тескт" @input="$emit('input', $event.target.value)")
+
+</template>
+
+<script>
+export default {
+	name: 'AppInput',
+	model: {
+		prop: 'value',
+		event: 'input'
+	}
+};
+</script>
+
+<style scoped>
+/* ads */
+</style>
